024b88fdeb9012ba1229e093be494b55e07f8a6e
Malalicious <info@roeldev.nl>
2017-04-13 12:07:15 +0200

    Ported "lockons" plugin over to Alkahest.

---
+++ Alkahest.Plugins.InstantLockon/InstantLockonPlugin.cs
@@ -0,0 +1,46 @@
+﻿using Alkahest.Core;
+using Alkahest.Core.Logging;
+using Alkahest.Core.Net;
+using Alkahest.Core.Net.Protocol.Packets;
+using Alkahest.Core.Plugins;
+using System.Linq;
+
+namespace Alkahest.Plugins.InstantLockon
+{
+    public sealed class InstantLockonPlugin : IPlugin
+    {
+        public string Name { get; } = "instantlockon";
+
+        static readonly Log _log = new Log(typeof(InstantLockonPlugin));
+
+        public void Start(GameProxy[] proxies)
+        {
+            foreach (var proc in proxies.Select(x => x.Processor))
+                proc.AddHandler<CCanLockOnTargetPacket>(HandleLockon);
+
+            _log.Basic("Instant Lockon plugin started");
+        }
+
+        public void Stop(GameProxy[] proxies)
+        {
+            foreach (var proc in proxies.Select(x => x.Processor))
+                proc.RemoveHandler<CCanLockOnTargetPacket>(HandleLockon);
+
+            _log.Basic("Instant Lockon plugin stopped");
+        }
+
+        bool HandleLockon(GameClient client, Direction direction,
+            CCanLockOnTargetPacket packet)
+        {
+            client.SendToClient(new SCanLockOnTargetPacket
+            {
+                CanLockOn = true,
+                Skill = packet.Skill,
+                Target = packet.Target,
+                Unknown1 = packet.Unknown1
+            });
+
+            return true;
+        }
+    }
+}
---
+++ Alkahest.Plugins.InstantLockon/README.md
@@ -0,0 +1,3 @@
+﻿# Instant Lockon Plugin
+
+Port from the [tera-proxy](https://github.com/meishuu/tera-proxy) mod [lockons](https://github.com/baldera-mods/lockons) by meishuu for Alkahest.
