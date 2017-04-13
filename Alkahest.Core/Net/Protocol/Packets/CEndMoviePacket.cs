f19167125f88fadb97691497b9574310b61fdc3d
Malalicious <info@roeldev.nl>
2017-04-13 12:38:46 +0200

    Added cutscene-related packets for a future plugin to skip cutscenes.

---
+++ Alkahest.Core/Net/Protocol/Packets/CEndMoviePacket.cs
@@ -0,0 +1,21 @@
+﻿namespace Alkahest.Core.Net.Protocol.Packets
+{
+    public sealed class CEndMoviePacket : Packet
+    {
+        const string Name = "C_END_MOVIE";
+
+        public override string OpCode => Name;
+
+        [Packet(Name)]
+        internal static Packet Create()
+        {
+            return new CEndMoviePacket();
+        }
+
+        [PacketField]
+        public uint Movie { get; set; }
+
+        [PacketField]
+        public bool Unknown1 { get; set; }
+    }
+}
